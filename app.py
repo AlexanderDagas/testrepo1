41cb08f8c7270db41d2b62800142d6db4b706d01
Alexander Dagas <dagasalexander@gmail.com>
2025-04-27 21:09:56 +0100

    Added Flask code.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    task_data = request.get_json()
+    task_text = task_data.get('task')
+    print(f"Received task: {task_text}")
+    return "Task received", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
